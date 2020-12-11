6096c13b94d451f343ff2f126c1a94e7e496ca08
teranasoras <tmayn@gmail.com>
2020-12-11 11:24:04 -0600

    finished

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,21 @@
 module.exports = {
   development: {
-    // Complete.
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/database.db',
+    },
     migrations: {
-      directory: "./data/migrations",
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
   },
-  testing: {
-    // Complete (otherwise `npm test` won't work in your local machine).
-    migrations: {
-      directory: "./data/migrations",
-    },
-  }
 };
